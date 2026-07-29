3afff70cc583a867be1787c7d076c6efcd01ed77
Jan Michalski <jan-marian.michalski@hpe.com>
2026-07-29 12:31:42 +0000

    common: add empty build.sh
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>

---
+++ projects/daos/build.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Copyright 2026 Hewlett Packard Enterprise Development LP
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
+# Syncs DAOS fuzz projects into an OSS-Fuzz checkout.
+#
+
+set -euo pipefail
