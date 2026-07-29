8c961e61ff5f6a05f03fb7a7ea5ab8306c5600d6
Jan Michalski <jan-marian.michalski@hpe.com>
2026-07-29 11:57:51 +0000

    helpers: sync.sh: add copyright and SPDX header
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>

--- projects/daos/sync.sh
+++ projects/daos/sync.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
+#
+# Copyright 2026 Hewlett Packard Enterprise Development LP
+#
+# SPDX-License-Identifier: BSD-2-Clause-Patent
+#
 
 ROOT_DIR=$(realpath "$(dirname "$0")/../..")
 
 echo $ROOT_DIR
 
 if [ ! -d "$1" ]; then
-    echo "Error: $1 is not a directory"
+    echo "Error: '$1' is not a directory"
     exit 1
 fi
 
