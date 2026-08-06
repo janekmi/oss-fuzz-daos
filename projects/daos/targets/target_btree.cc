b1392c924fa6843efbeabbc0fd35fee6c3e65bcf
Jan Michalski <jan-marian.michalski@hpe.com>
2026-08-06 18:30:51 +0000

    common: fix some warnings
    Signed-off-by: Jan Michalski <jan-marian.michalski@hpe.com>

--- projects/daos/targets/target_btree.cc
+++ projects/daos/targets/target_btree.cc
@@ -30,8 +30,8 @@ DEFINE_PROTO_FUZZER(const target_btree::Msg& input) {
 		switch (cmd.cmd_case()) {
 		case target_btree::Msg_Command::kCreate: {
 			const auto& c = cmd.create();
-			syslog(LOG_INFO, "Create: feat_uint_key=%lu feat_embed_first=%lu inplace=%lu order=%u",
-			       c.feat_uint_key(), c.feat_embed_first(), c.inplace(), c.order());
+			syslog(LOG_INFO, "Create: feat_uint_key=%c feat_embed_first=%c inplace=%c order=%u",
+			       (char)c.feat_uint_key(), (char)c.feat_embed_first(), (char)c.inplace(), c.order());
 			tb_create_cmd(c.feat_uint_key(), c.feat_embed_first(), c.inplace(), c.order());
 			break;
 		}
